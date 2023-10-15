b7dd7216a8abaf4639a12c15348ebb501827cc6d
Suman Shekhar <suman.sumanshekhar698@gmail.com>
2023-10-15 14:31:37 +0530

    updated

--- src/com/codecounty/dsa/recursion/MergeSort.java
+++ src/com/codecounty/dsa/recursion/MergeSort.java
@@ -1,25 +1,30 @@
 package com.codecounty.dsa.recursion;
 
+import java.util.Arrays;
+
 public class MergeSort {// if you have a single element in the array its sorted
 
 	public static void main(String[] args) {
 
 		int arr[] = { 38, 27, 43, 10 };
 		sort(arr, 0, arr.length - 1);
+		System.out.println(Arrays.toString(arr));
 
 	}
 
 	private static void sort(int[] arr, int l, int h) {
-		
+
 //		O(n) time = log n
 		if (l < h) {// base condition
 //			int mid = (l+h)/2;
 			int mid = l + (h - l) / 2;
+
+			// sorting 1st and 2nd half
 			sort(arr, l, mid);
 			sort(arr, mid + 1, h);
 
+			// merge the sorted halves
 			merge(arr, l, mid, h);
-
 		}
 
 	}
@@ -64,7 +69,9 @@ public class MergeSort {// if you have a single element in the array its sorted
 		}
 
 		while (j < q) {
-			arr[k] = qArr[k];
+//			arr[k] = qArr[k];//Typo
+			arr[k] = qArr[j];
+
 			++j;
 			++k;
 		}
